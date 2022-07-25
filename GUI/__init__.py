e3ea4a27c524fe93cf637cefd2dd9df73b296203
Backist <alvarodrumer54@gmail.com>
2022-07-25 23:45:44 +0200

    Fixing relative imports

---
+++ GUI/__init__.py
@@ -0,0 +1,3 @@
+#Adding __init__.py to the folder will make it a package
+#putting __init__.py in a directory (or subdirectory) will make it a package
+#__init__.py is a special file that Python runs when it encounters a package
